014741ac0c15478ff36acfe4e898228d0cac4e16
Lusala2015 <lusalamark17@gmail.com>
2020-05-21 14:27:18 +0300

    First php commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+echo ("Hello World");
+echo "<h1> Mark is the programmer </h1>";
+echo "<hr>";
+echo "<p> Mark is the programmer </p>";
+echo "<h1> Mark is the programmer </h1>";
+
+?>
+</body>
+</html>
